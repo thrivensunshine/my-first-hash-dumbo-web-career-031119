de7a6dad505be8a67a6f6166582ef83c62cc1825
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-24 16:48:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+my_hash = {"author" => "octavia butler", "book" => "seeds to harvest"}
   
 end
 
